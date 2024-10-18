729e3bb2df50b43b2d6b3d51d26de8897a0b9da3
Manuel Fernandez <manufer6503@gmail.com>
2024-10-18 12:40:02 -0300

    feat: create routes object for site navigation

---
+++ teleferico-app/src/utils/routes.const.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  HOME: "/",
+  INFO: "/info",
+  ACTIVITIES: "/activities",
+  GALLERY: "/gallery",
+  NEWS: "/news",
+  JOBS: "/jobs",
+  RULES: "/rules",
+  CONTACT: "/contact",
+  LEGALS: "/legals",
+};
