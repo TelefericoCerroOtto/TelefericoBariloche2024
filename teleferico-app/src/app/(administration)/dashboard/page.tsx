20e17748b385135d05edaca9e64cda1cd885ebb0
Manuel Fernandez <manufer6503@gmail.com>
2024-11-20 12:04:42 -0300

    feat(pages): create dashboard home page

--- teleferico-app/src/app/(administration)/dashboard/page.tsx
+++ teleferico-app/src/app/(administration)/dashboard/page.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import clockIilustration from "@/public/oc-time-flies.svg";
+
 export default async function DashboardPage() {
   return (
-    <div>
-      <h1 className="font-bold">Dashboard page</h1>
+    <div className="w-max-[1350px] flex h-[400px] w-full items-center px-10">
+      <div className="flex flex-col gap-3">
+        <p className="text-custom-red">Hola Manuel</p>
+        <h2 className="text-4xl font-bold">
+          Bienvenido al Panel de Administración
+        </h2>
+        <p className="font-light">
+          Este panel de control está diseñado para{" "}
+          <span className="font-bold">facilitar el trabajo del equipo,</span>{" "}
+          ofreciendo herramientas prácticas para cumplir funciones de
+          administración, reclutamiento y gestión de contenido multimedia. Cada
+          usuario podrá acceder a los recursos necesarios para desempeñar sus
+          tareas.
+        </p>
+      </div>
+      <Image
+        src={clockIilustration}
+        alt="ilustracion vectorizada reloj"
+        width={400}
+      />
     </div>
   );
 }
