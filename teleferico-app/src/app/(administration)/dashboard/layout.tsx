2c520d17b914ddd8256917867efc2ca7121ab173
Manuel Fernandez <manufer6503@gmail.com>
2024-11-27 15:33:49 -0300

    style: responsive design of layout and dashboard page

--- teleferico-app/src/app/(administration)/dashboard/layout.tsx
+++ teleferico-app/src/app/(administration)/dashboard/layout.tsx
@@ -11,13 +11,15 @@ export default function DashboardLayout({
     <div>
       <SidebarProvider>
         <Sidebar />
-        <main className="w-full">
+        <main className="w-full max-w-full">
           <Header />
           {/* TODO: resolver diferencia con el height del header.
           tailwind no acepta clases de manera dinamica 
           https://stackoverflow.com/questions/71791472/fontawesome-icons-not-accepting-color-props-through-react-functional-components/
           */}
-          <div className={`h-[calc(100%-75px)] w-full bg-accent`}>
+          <div
+            className={`h-[calc(100%-75px)] w-full max-w-full bg-accent pt-5`}
+          >
             {children}
           </div>
         </main>
--- teleferico-app/src/app/(administration)/dashboard/page.tsx
+++ teleferico-app/src/app/(administration)/dashboard/page.tsx
@@ -3,7 +3,7 @@ import clockIilustration from "@/public/oc-time-flies.svg";
 
 export default async function DashboardPage() {
   return (
-    <div className="w-max-[1350px] flex h-[400px] w-full items-center px-10">
+    <div className="w-max-[1350px] flex w-full flex-col-reverse items-center px-10 md:h-[400px] md:flex-row">
       <div className="flex flex-col gap-3">
         <p className="text-custom-red">Hola Manuel</p>
         <h2 className="text-4xl font-bold">
