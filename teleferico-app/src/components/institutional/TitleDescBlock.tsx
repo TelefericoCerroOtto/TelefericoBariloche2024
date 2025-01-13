73d90b0ca30681a57030c3296aefbb7daa11f261
Manuel Fernandez <manufer6503@gmail.com>
2025-01-13 14:04:03 -0300

    styles: improve TitleDescBlock styles

--- teleferico-app/src/components/institutional/TitleDescBlock.tsx
+++ teleferico-app/src/components/institutional/TitleDescBlock.tsx
@@ -17,7 +17,7 @@ export default function TitleDescBlock(props: Props) {
 
   return (
     <div
-      className={`flex max-w-[800px] flex-col ${alignVariants[align]} mx-auto mb-12 gap-5 px-10 py-2 md:px-16`}
+      className={`flex max-w-[800px] flex-col ${alignVariants[align]} mx-auto mb-12 gap-5 px-8 md:px-12`}
     >
       {epigraph ? <p className="text-small text-primary">{epigraph}</p> : null}
       <h4 className="text-3xl font-bold capitalize text-inherit md:text-5xl">
