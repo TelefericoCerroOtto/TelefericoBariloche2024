ddede9fcc77a38ae4a8ceed537b0b0b9a636d142
Manuel Fernandez <manufer6503@gmail.com>
2025-01-19 13:13:32 -0300

    feat(components): create ContactInfo comp

--- teleferico-app/src/app/(institutional)/jobs/page.tsx
+++ teleferico-app/src/app/(institutional)/jobs/page.tsx
@@ -1,4 +1,4 @@
-import { TitleDescBlock } from "@/components";
+import { ContactInfo, TitleDescBlock } from "@/components";
 import { Spacer } from "@nextui-org/react";
 import { Form } from "./_components";
 
@@ -15,21 +15,7 @@ export default function JobsPage() {
         />
         <div className="flex flex-col gap-8 lg:flex-row">
           <Form />
-          <TitleDescBlock
-            title="Informacion de contacto"
-            desc={
-              <>
-                <span className="block">
-                  <strong>Direccion:</strong> Av. De los Pioneros KM 5.000, San
-                  Carlos De Bariloche, Rio Negro, Argentina
-                </span>
-                <span className="block">
-                  <strong>Telefono:</strong> +54 294 4441 1031
-                </span>
-              </>
-            }
-            align="start"
-          />
+          <ContactInfo />
         </div>
       </div>
     </>
--- teleferico-app/src/components/institutional/index.ts
+++ teleferico-app/src/components/institutional/index.ts
@@ -1,7 +1,8 @@
+export { default as ContactInfo } from "./ContactInfo";
 export { default as Hero } from "./Hero";
+export { default as HoursOverview } from "./HoursOverview";
+export { default as HoursOverviewItems } from "./HoursOverviewItems";
 export { default as ImageTextRenderer } from "./ImageTextSection/ImageTextRenderer";
 export { default as ImageTextSection } from "./ImageTextSection/ImageTextSection";
-export { default as TitleDescBlock } from "./TitleDescBlock";
-export { default as HoursOverviewItems } from "./HoursOverviewItems";
-export { default as HoursOverview } from "./HoursOverview";
 export { default as PageWrapper } from "./PageWrapper";
+export { default as TitleDescBlock } from "./TitleDescBlock";
