ef9b6a93f912892b7e9a45da4dfc1ef1cd40e3bb
Manuel Fernandez <manufer6503@gmail.com>
2025-02-01 19:57:12 -0300

    feat(app/pages): replace server-side signout with client-side signout

--- teleferico-app/src/app/(administration)/logout/page.tsx
+++ teleferico-app/src/app/(administration)/logout/page.tsx
@@ -1,7 +1,7 @@
-import LogoutForm from "./LogoutForm";
+import Logout from "./Logout";
 
-// Logout page will autosubmit a signout form, which deletes Authjs session.
+// The logout page renders the Logout component, which automatically signs out the user, deleting the Auth.js session.
 
 export default function LogoutPage() {
-  return <LogoutForm />;
+  return <Logout />;
 }
