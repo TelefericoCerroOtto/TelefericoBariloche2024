be009e8c1a4c4b27fbff2755a5a83b24fd621910
Manuel Fernandez <manufer6503@gmail.com>
2025-02-13 19:07:37 -0300

    refactor(app/services): create get content function to activities page & abstract query constructor

--- teleferico-app/src/lib/services/pages.ts
+++ teleferico-app/src/lib/services/pages.ts
@@ -3,31 +3,42 @@ import type { GetHomePage, Locales } from "@/types";
 import { fetchWrapper } from "@/utils/fetch";
 import { getStrapiURL } from "@/utils/get-strapi-url";
 import { stringifyQuery } from "@/utils/query";
-import { STRAPI_ENDPOINTS } from "@/utils/routes.const";
+import { ROUTES, STRAPI_ENDPOINTS } from "@/utils/routes.const";
 
-export const getHomePageContent = async (locale: Locales) => {
-  const query = {
-    locale: locale ?? i18n.defaultLocale,
-    filters: {
-      route: {
-        $eq: "/",
-      },
+const getQuery = (locale: Locales, route: string) => ({
+  locale: locale ?? i18n.defaultLocale,
+  filters: {
+    route: {
+      $eq: route,
     },
-    populate: {
-      blocks: {
-        on: {
-          "page-components.hero": {
-            populate: ["firstLink", "secondLink", "cover.image"],
-          },
-          "page-components.hours-overview": "*",
-          "page-components.title-desc-block": "*",
-          "page-components.image-text-block": {
-            populate: "images.image",
-          },
+  },
+  populate: {
+    blocks: {
+      on: {
+        "page-components.hero": {
+          populate: ["firstLink", "secondLink", "cover.image"],
+        },
+        "page-components.hours-overview": "*",
+        "page-components.title-desc-block": "*",
+        "page-components.image-text-block": {
+          populate: ["images.image", "link"],
         },
       },
     },
-  };
+  },
+});
+
+export const getHomePageContent = async (locale: Locales) => {
+  const query = getQuery(locale, ROUTES.HOME);
+  const res = await fetchWrapper<GetHomePage>(
+    getStrapiURL(STRAPI_ENDPOINTS.PAGES, stringifyQuery(query)),
+  );
+
+  return res;
+};
+
+export const getActivitiesPageContent = async (locale: Locales) => {
+  const query = getQuery(locale, ROUTES.ACTIVITIES);
 
   const res = await fetchWrapper<GetHomePage>(
     getStrapiURL(STRAPI_ENDPOINTS.PAGES, stringifyQuery(query)),
