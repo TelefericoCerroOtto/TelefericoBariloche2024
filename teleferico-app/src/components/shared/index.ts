152bea97e82b61df19a6838c1a91aa0e7aad3f8f
Manuel Fernandez <manufer6503@gmail.com>
2025-02-13 22:27:14 -0300

    chore(app/components): move FormError to shared components sub dir

--- teleferico-app/src/components/shared/index.ts
+++ teleferico-app/src/components/shared/index.ts
@@ -1,6 +1,7 @@
 export { default as BlockRendererClient } from "./BlockRendererClient";
 export { default as ButtonDos } from "./ButtonDos";
 export { default as CustomLink } from "./CustomLink";
+export { default as FormError } from "./FormError";
 export { default as Input } from "./Input";
 export { default as StrapiComponentRenderer } from "./StrapiComponentRenderer";
 export { default as TableContainer } from "./TableContainer";
