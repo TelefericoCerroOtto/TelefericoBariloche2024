27dd55d7b046f6dad91847a84437af25aafaffad
Manuel Fernandez <manufer6503@gmail.com>
2025-02-13 21:35:52 -0300

    feat(app/i18n): add internationalization to contact page

--- teleferico-app/src/app/(institutional)/[locale]/contact/_components/Form.tsx
+++ teleferico-app/src/app/(institutional)/[locale]/contact/_components/Form.tsx
@@ -1,27 +1,32 @@
 "use client";
 
 import { ButtonDos, Input } from "@/components";
+import { useTranslation } from "@/hooks";
 import { Textarea } from "@nextui-org/react";
 
 export default function Form() {
+  const { t } = useTranslation();
+  const formIntl = t("components.Forms");
+
   return (
     <form className="grid flex-grow grid-cols-1 gap-4">
       <Input id="name" name="name" label="Nombre" placeholder="Nombre" />
       <Input
         id="email"
         name="email"
-        label="Email"
-        placeholder="Email"
+        label={formIntl.fields["email"].label}
+        placeholder={formIntl.fields["email"].placeholder}
         type="email"
       />
       <Textarea
-        className=""
-        label="Consulta"
-        placeholder="Ingresa tu consulta"
+        id="consultation"
+        name="consultation"
+        label={formIntl.fields["consultation"].label}
+        placeholder={formIntl.fields["consultation"].placeholder}
         labelPlacement="outside"
       />
       <ButtonDos type="submit" className="w-[90px]">
-        Enviar
+        {formIntl.sendbtn}
       </ButtonDos>
     </form>
   );
--- teleferico-app/src/app/(institutional)/[locale]/contact/page.tsx
+++ teleferico-app/src/app/(institutional)/[locale]/contact/page.tsx
@@ -1,21 +1,31 @@
-import { ContactInfo, TitleDescBlock } from "@/components";
+import { BlocksRenderer } from "@/components";
+import { getPageContent } from "@/lib/services/pages";
+import type { Locales } from "@/types";
+import { ROUTES } from "@/utils/routes.const";
 import { Spacer } from "@nextui-org/react";
 import { Form } from "./_components";
 
-export default function ContactPage() {
+export default async function ContactPage({
+  params,
+}: Readonly<{
+  params: Promise<{ locale: Locales }>;
+}>) {
+  const { locale } = await params;
+
+  const res = await getPageContent(locale, ROUTES.CONTACT);
+  // TODO: COMPLETE FALLBACK DATA FROM GETTING CONTENT PAGES
+  if (!res.ok) return <div>Fallback data</div>;
+
+  const blocks = res.data.data[0].blocks;
+
   return (
     <>
       <Spacer y={28} />
       <div className="flex w-full flex-col px-10 lg:px-28">
-        <TitleDescBlock
-          title="Medios De Contacto"
-          desc="Queremos escucharte y responder a todas tus dudas"
-          epigraph="Contactanos fácil"
-          align="start"
-        />
+        <BlocksRenderer blocks={blocks[0]} locale={locale} />
         <div className="flex flex-col gap-8 lg:flex-row">
           <Form />
-          <ContactInfo />
+          <BlocksRenderer blocks={blocks[1]} locale={locale} />
         </div>
       </div>
     </>
