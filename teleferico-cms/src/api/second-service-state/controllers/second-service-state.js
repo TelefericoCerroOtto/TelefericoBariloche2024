36edbb3921ec7f284649bd78aa704726a414383a
Manuel Fernandez <manufer6503@gmail.com>
2025-04-01 15:00:37 -0300

    feat(cms/collections): delete unused collection type and components

--- teleferico-cms/src/api/second-service-state/controllers/second-service-state.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * second-service-state controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::second-service-state.second-service-state');
--- teleferico-cms/src/api/second-service-state/services/second-service-state.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * second-service-state service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::second-service-state.second-service-state');
