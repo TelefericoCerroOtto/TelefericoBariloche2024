097eefcc80de3bf814923da1d6ed82e37b1bf646
Manuel Fernandez <manufer6503@gmail.com>
2025-04-01 15:02:28 -0300

    feat(cms/collections): create SectorName collection type

---
+++ teleferico-cms/src/api/sector-name/controllers/sector-name.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sector-name controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sector-name.sector-name');
