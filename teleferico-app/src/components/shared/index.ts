aa22b077edc8a036ffb6a738a5f578c82ac1db1f
Manuel Fernandez <manufer6503@gmail.com>
2025-04-01 15:30:34 -0300

    feat(app/components): create InputSkeleton component

--- teleferico-app/src/components/shared/index.ts
+++ teleferico-app/src/components/shared/index.ts
@@ -3,5 +3,6 @@ export { default as ButtonDos } from "./ButtonDos";
 export { default as CustomLink } from "./CustomLink";
 export { default as FormError } from "./FormError";
 export { default as Input } from "./Input";
+export { default as InputSkeleton } from "./InputSkeleton";
 export { default as StrapiComponentRenderer } from "./StrapiComponentRenderer";
 export { default as TableContainer } from "./TableContainer";
