3c53ef94bbc1fa570c81cec5503007f8148db893
Manuel Fernandez <manufer6503@gmail.com>
2025-04-28 11:50:56 -0300

    feat(app/next.config): allow loading images from Strapi bucket

--- teleferico-app/next.config.mjs
+++ teleferico-app/next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.STRAPI_BUCKET_HOSTNAME,
+        pathname: process.env.STRAPI_BUCKET_PATHNAME,
+      },
+    ],
+  },
   // This rewrite rule proxies image requests through Next.js to hide the Strapi base URL.
   // This allows frontend components to load images without directly referencing the backend URL.
   async rewrites() {
