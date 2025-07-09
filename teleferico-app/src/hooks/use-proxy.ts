2a968be83223ebc3997b4b1376246968163fb578
Manuel Fernandez <manufer6503@gmail.com>
2025-07-08 22:13:19 -0300

    feat(app/hooks): add optional swr configuration param to useProxy custom hook

--- teleferico-app/src/hooks/use-proxy.ts
+++ teleferico-app/src/hooks/use-proxy.ts
@@ -1,13 +1,19 @@
 import { fetcher } from "@/utils/fetcher";
 import { stringifyQuery } from "@/utils/query";
 import { ROUTE_HANDLERS } from "@/utils/routes.const";
-import useSWR from "swr";
+import useSWR, { type BareFetcher, type SWRConfiguration } from "swr";
 
-export function useProxy<T>(endpoint: string, qs: object) {
+export function useProxy<T>(
+  endpoint: string,
+  qs: object,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  swrconfig?: SWRConfiguration<T, any, BareFetcher<T>>,
+) {
   const queryParams = stringifyQuery(qs);
   const key = `${ROUTE_HANDLERS.PROXY}${endpoint}?${queryParams}`;
 
   const { data, error, isLoading } = useSWR<T>(key, fetcher, {
+    ...swrconfig,
     errorRetryCount: 2,
     errorRetryInterval: 5000,
   });
