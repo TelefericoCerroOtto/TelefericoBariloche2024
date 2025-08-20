09fe7749c1b816e03a524374dbf90636298ae2b4
Manuel Fernandez <manufer6503@gmail.com>
2025-08-20 15:04:31 -0300

    fix(app/hooks): allow optional query params in useProxy custom hook

--- teleferico-app/src/hooks/use-proxy.ts
+++ teleferico-app/src/hooks/use-proxy.ts
@@ -5,12 +5,12 @@ import useSWR, { type BareFetcher, type SWRConfiguration } from "swr";
 
 export function useProxy<T>(
   endpoint: string,
-  qs: object,
+  qs?: object,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   swrconfig?: SWRConfiguration<T, any, BareFetcher<T>>,
 ) {
-  const queryParams = stringifyQuery(qs);
-  const key = `${ROUTE_HANDLERS.PROXY}${endpoint}?${queryParams}`;
+  const queryParams = qs ? stringifyQuery(qs) : "";
+  const key = `${ROUTE_HANDLERS.PROXY}${endpoint}${queryParams ? `?${queryParams}` : queryParams}`;
 
   const { data, error, isLoading } = useSWR<T>(key, fetcher, {
     ...swrconfig,
