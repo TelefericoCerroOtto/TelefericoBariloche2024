51f97a87e8aae45dd0a7c10f7f5bdf35baac832b
Manuel Fernandez <manufer6503@gmail.com>
2025-09-25 12:30:24 -0300

    refactor(app/hooks): rename use-mobile.ts to use-is-mobile.ts

--- teleferico-app/src/hooks/index.ts
+++ teleferico-app/src/hooks/index.ts
@@ -1,6 +1,6 @@
 export * from "./use-form-locale-selector";
 export * from "./use-locale";
-export * from "./use-mobile";
+export * from "./use-is-mobile";
 export * from "./use-proxy";
 export * from "./use-service-state";
 export * from "./use-translation";
