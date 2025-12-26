42e0d206db040e9116ab925e6a3a59b912bf744b
Manuel Fernandez <manufer6503@gmail.com>
2025-12-26 02:08:25 -0300

    fix(app/components): point schema imports to new barrel in LoginForm component

--- teleferico-app/src/app/[locale]/(administration)/login/_components/LoginForm.tsx
+++ teleferico-app/src/app/[locale]/(administration)/login/_components/LoginForm.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { ButtonDos, FormError } from "@/components";
-import { loginSchema } from "@/lib/schemas/forms";
-import type { LoginFormData, LoginUserRequest } from "@/types";
 import { ADMIN_ROUTES } from "@/lib/constants/routes.const";
 import { formInputClassNames } from "@/lib/constants/styles.const";
+import { loginSchema } from "@/lib/schemas";
+import type { LoginFormData, LoginUserRequest } from "@/types";
 import { Input, Spinner } from "@heroui/react";
 import { useFormik } from "formik";
 import { Eye, EyeOff } from "lucide-react";
