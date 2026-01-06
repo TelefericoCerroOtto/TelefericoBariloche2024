b5912b4e009b73ca20075da36889f6af29066f9b
Manuel Fernandez <manufer6503@gmail.com>
2026-01-06 16:28:13 -0300

    feat(app/components): delete deprecated StrapiComponentRenderer

--- teleferico-app/src/components/shared/index.ts
+++ teleferico-app/src/components/shared/index.ts
@@ -2,5 +2,5 @@ export { default as BlockRendererClient } from "./BlockRendererClient";
 export { default as ButtonDos } from "./ButtonDos";
 export { default as CustomLink } from "./CustomLink";
 export { default as FormError } from "./FormError";
-export { default as StrapiComponentRenderer } from "./StrapiComponentRenderer";
+export { default as StrapiComponentRenderer } from "./StrapiComponentRenderer/index";
 export { default as TableContainer } from "./TableContainer";
