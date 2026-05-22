2918a9e7c5b8d83ed305b4651279818073773a1c
Manuel Fernandez <manufer6503@gmail.com>
2026-05-22 14:54:42 -0300

    feat(app-cms/form-protection): add form protection submission content type

---
+++ teleferico-cms/src/api/form-protection-submission/controllers/form-protection-submission.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * form-protection-submission controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController(
+  'api::form-protection-submission.form-protection-submission',
+);
---
+++ teleferico-cms/src/api/form-protection-submission/routes/form-protection-submission.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * form-protection-submission router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter(
+  'api::form-protection-submission.form-protection-submission',
+);
